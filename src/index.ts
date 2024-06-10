475a6b95b57712dfbd6fdd8a14ae7d6c2e6ee84b
Iñaki Arango <arangoinaki@gmail.com>
2024-06-10 02:50:27 -0400

    Updated the host to comply with render

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './app';
 
 const FASTIFY_PORT = Number(process.env.PORT) || 3006;
 
-app.listen({ port: FASTIFY_PORT });
+app.listen({ host: '0.0.0.0', port: FASTIFY_PORT });
 
 console.log(
   `🚀  Fastify server running on port http://localhost:${FASTIFY_PORT}`,
